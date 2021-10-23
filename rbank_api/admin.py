1957e1f8d771b297ff31daee47a2d3bf0848002f
Barry <blrussell09@gmail.com>
2021-10-23 17:40:28 -0500

    Added models for transactions, shares, and members. I also updated the account model

--- rbank_api/admin.py
+++ rbank_api/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Account
+from .models import Account, Member, Share, Transaction
 # Register your models here.
 
 admin.site.register(Account)
+admin.site.register(Member)
+admin.site.register(Share)
+admin.site.register(Transaction)
