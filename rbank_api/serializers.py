143c2b8c11851e0aa6bc7efbf42e69446a13cca6
Barry <blrussell09@gmail.com>
2021-11-07 14:17:55 -0600

    Added new api urls for accounts, and will allow queries of accounts by member number

--- rbank_api/serializers.py
+++ rbank_api/serializers.py
@@ -1,7 +1,13 @@
 from  rest_framework import serializers
 from .models import Member
+from .models import Account
 
 class MemberSerializer(serializers.HyperlinkedModelSerializer):
   class Meta:
     model = Member
     fields = ('id','first_name', 'last_name', 'birthday', 'ssn', 'address_one', 'address_two', 'city', 'state', 'zip_code')
+
+class AccountSerializer(serializers.HyperlinkedModelSerializer):
+  class Meta:
+    model = Account
+    fields = ('id','open_date', 'close_date', 'activity_date', 'member_id')
--- rbank_api/views.py
+++ rbank_api/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import MemberSerializer
-from .models import Member
+from .serializers import MemberSerializer, AccountSerializer
+from .models import Member, Account
 # Create your views here.
 
 class MemberViewSet(viewsets.ModelViewSet):
   queryset = Member.objects.all().order_by('first_name')
   serializer_class = MemberSerializer
+
+class AccountViewSet(viewsets.ModelViewSet):
+  serializer_class = AccountSerializer
+
+  def get_queryset(self):
+    queryset = Account.objects.all()
+    member_num = self.request.query_params.get('member_num')
+    if member_num is not None:
+      queryset = queryset.filter(member_id=member_num)
+    return queryset
